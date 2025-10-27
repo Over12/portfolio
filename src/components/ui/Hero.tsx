c9a6549e0a1e7cb237b0512217fea188e896a35b
Over12 <avalosfigueroapedro@gmail.com>
2025-10-27 18:13:31 -0500

    feat: agregar imagen de avatar en el componente Hero

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -1,7 +1,15 @@
+import { motion } from 'motion/react'
+
 export default function Hero() {
   return (
-    <section className='min-h-screen'>
-
+    <section className='h-dvh cursor-none'>
+      <div className='relative flex flex-col items-start justify-between h-screen p-10'>
+        <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} className='text-pretty w-1/2 text-4xl mt-48'>Creando experiencias web modernas, rápidas y funcionales con un enfoque en diseño limpio y rendimiento.</motion.p>
+        <motion.p initial={{ opacity: 0 }} animate={{ opacity: 1 }} className='w-1/3 text-text/50'><span className='text-text font-medium'>Desarrollador web</span> y <span className='text-text font-medium'>ser humano</span>, enfocado en crear experiencias digitales que inspiren conexión.</motion.p>
+        <motion.div initial={{ opacity: 0, height: '200px' }} animate={{ opacity: 1, height: '348px' }} className='absolute bottom-10 right-10 w-60 h-96 bg-primary/90 overflow-hidden transition-colors'>
+            <img src='/src/assets/images/avatar.webp' alt='Imagen de avatar' className='size-full object-cover' />
+        </motion.div>
+      </div>
     </section>
   )
 };
