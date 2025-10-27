48606d1d253506492838f12b7444fa3b2a7b596f
Over12 <avalosfigueroapedro@gmail.com>
2025-10-27 18:13:01 -0500

    fix: corregir estilos de Navbar y ajustar clases de cursor

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -9,18 +9,17 @@ export default function Navbar() {
 
   const links = [
     { to: '/', label: 'Inicio' },
-    { to: '/tecnologias', label: 'Tecnologías' },
     { to: '/proyectos', label: 'Proyectos' },
     { to: '/contacto', label: 'Contacto' },
   ]
 
   return (
-    <nav className='fixed grid grid-cols-3 inset-x-0 px-10 py-5 z-50'>
-      <p className='text-3xl font-medium'>over.code</p>
+    <nav className='fixed grid grid-cols-3 inset-x-0 px-10 py-5 z-50 cursor-none'>
+      <p className='text-3xl font-medium'>pedro.avalos</p>
       <ul className='flex w-fit place-self-center items-center justify-center gap-5'>
         {links.map(({ to, label }) => (
           <li key={to} className='relative'>
-            <Link to={to} className='p-2'>{label}</Link>
+            <Link to={to} className='p-2 cursor-none'>{label}</Link>
 
             {pathname === to && (
               <motion.span layoutId='nav-indicator' transition={{ type: 'spring', stiffness: 700, damping: 30 }} className='absolute inset-x-0 bottom-0 h-0.5 bg-primary'/>
@@ -28,7 +27,7 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
-      <motion.button whileTap={{ rotate: -90 }} onClick={toggleTheme} className='place-self-end p-2 cursor-pointer focus:outline-none'>
+      <motion.button whileTap={{ rotate: -90 }} onClick={toggleTheme} className='place-self-end p-2 cursor-none focus:outline-none'>
         {theme === 'dark' ? <IconMoonFilled /> : <IconSunFilled />}
       </motion.button>
     </nav>
