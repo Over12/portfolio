0b1b892f15d1d319c0948e2c310e64a685b6c44b
Over12 <avalosfigueroapedro@gmail.com>
2025-12-26 09:40:54 -0500

    feat: Implementar estilos para vistas mobile a componentes Navbar y Hero

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -3,10 +3,10 @@ import { motion } from 'motion/react'
 export default function Hero() {
   return (
     <section className='h-dvh cursor-none'>
-      <div className='relative flex flex-col items-start justify-between h-screen p-10'>
-        <motion.p initial={{ opacity: 0, x: -50 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -15 }} className='text-pretty w-1/2 text-4xl mt-48'>Transformando ideas en experiencias digitales modernas, intuitivas y de alto rendimiento.</motion.p>
-        <motion.p initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 15 }} className='w-1/3 text-text/50'><span className='text-text font-medium'>Desarrollador web</span> y <span className='text-text font-medium'>ser humano</span>. Construyendo el futuro web, una línea de código a la vez.</motion.p>
-        <motion.div initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: 15 }} className='absolute bottom-10 right-10 w-60 h-96 bg-primary/90 overflow-hidden transition-colors'>
+      <div className='relative flex flex-col items-start justify-between h-screen p-5 sm:p-10'>
+        <motion.p initial={{ opacity: 0, x: -50 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: -15 }} className='text-pretty sm:w-1/2 text-3xl mt-36 sm:text-4xl sm:mt-48'>Transformando ideas en experiencias digitales modernas, intuitivas y de alto rendimiento.</motion.p>
+        <motion.p initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: 15 }} className='text-sm sm:text-base w-1/2 sm:w-1/3 text-text/50'><span className='text-text font-medium'>Desarrollador web</span> y <span className='text-text font-medium'>ser humano</span>. Construyendo el futuro web, una línea de código a la vez.</motion.p>
+        <motion.div initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} exit={{ opacity: 0, x: 15 }} className='absolute bottom-5 right-5 sm:bottom-10 sm:right-10 w-40 h-72 sm:w-60 sm:h-96 bg-primary/90 overflow-hidden transition-colors'>
             <img src='/src/assets/images/avatar.webp' alt='Imagen de avatar' className='size-full object-cover' />
         </motion.div>
       </div>
--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,11 +1,13 @@
-import { IconMoonFilled, IconSunFilled } from '@tabler/icons-react'
+import { IconMenuDeep, IconMoonFilled, IconSunFilled } from '@tabler/icons-react'
 import useTheme from '../../hooks/useTheme'
 import { Link, useLocation } from 'react-router'
-import { motion } from 'motion/react'
+import { AnimatePresence, motion } from 'motion/react'
+import { useState } from 'react'
 
 export default function Navbar() {
   const { theme, toggleTheme } = useTheme()
   const { pathname } = useLocation()
+  const [isOpen, setIsOpen] = useState<boolean>(false)
 
   const links = [
     { to: '/', label: 'Inicio' },
@@ -13,23 +15,43 @@ export default function Navbar() {
     { to: '/contacto', label: 'Contacto' },
   ]
 
-  return (
-    <nav className='absolute grid grid-cols-3 inset-x-0 px-10 py-5 z-50 cursor-none'>
-      <p className='text-3xl font-medium'>pedro.avalos</p>
-      <ul className='flex w-fit place-self-center items-center justify-center gap-5'>
-        {links.map(({ to, label }) => (
-          <li key={to} className='relative'>
-            <Link to={to} className='p-2 cursor-none'>{label}</Link>
+  const toogleMenu = () => {
+    setIsOpen(!isOpen)
+  }
 
-            {pathname === to && (
-              <motion.span layoutId='nav-indicator' initial={false} transition={{ type: 'spring', stiffness: 700, damping: 30 }} className='absolute inset-x-0 bottom-0 h-0.5 bg-primary'/>
-            )}
-          </li>
-        ))}
-      </ul>
-      <motion.button whileTap={{ rotate: -90 }} onClick={toggleTheme} className='place-self-end p-2 cursor-none focus:outline-none'>
-        {theme === 'dark' ? <IconMoonFilled /> : <IconSunFilled />}
-      </motion.button>
-    </nav>
+  return (
+    <>
+      <nav className='absolute flex justify-between items-center sm:grid sm:grid-cols-3 inset-x-0 px-5 sm:px-10 py-5 z-50 cursor-none'>
+        <p className='text-3xl font-medium'>pedro.avalos</p>
+        <ul className={`hidden sm:flex w-fit place-self-center items-center justify-center gap-5`}>
+          {links.map(({ to, label }) => (
+            <li key={to} className='relative'>
+              <Link to={to} className='p-2 cursor-none'>{label}</Link>
+
+              {pathname === to && (
+                <motion.span layoutId='nav-indicator' initial={false} transition={{ type: 'spring', stiffness: 700, damping: 30 }} className='absolute inset-x-0 bottom-0 h-0.5 bg-primary' />
+              )}
+            </li>
+          ))}
+        </ul>
+        <motion.button whileTap={{ rotate: -90 }} onClick={toggleTheme} className='hidden sm:block sm:place-self-end p-2 cursor-none focus:outline-none'>
+          {theme === 'dark' ? <IconMoonFilled /> : <IconSunFilled />}
+        </motion.button>
+        <button onClick={toogleMenu} className='sm:hidden p-2 cursor-none focus:outline-none'>
+          <IconMenuDeep className='size-8' />
+        </button>
+      </nav>
+      <AnimatePresence mode='wait'>
+        {isOpen && (
+          <motion.ul initial={{ opacity: 0, y: -20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, y: -20 }} className='sm:hidden absolute inset-x-0 top-24 px-5 flex justify-between items-center z-30'>
+            {links.map(({ to, label }) => (
+              <li key={to}>
+                <Link to={to} className='p-2'>{label}</Link>
+              </li>
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
+    </>
   )
 }
